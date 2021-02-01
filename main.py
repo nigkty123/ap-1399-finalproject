ae6f2c65928842ac45d0b4a37d77cc6653dd7b60
Negin Naserzadeh <72472009+nigkty123@users.noreply.github.com>
2021-02-01 12:23:26 +0330

    Bases Added

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+import os
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-app = QApplication(sys.argv)
+form = uic.loadUiType(os.path.join(os.getcwd(), "form.ui"))[0]
 
-w = QWidget()
-w.resize(1500, 1500)
-w.move(300, 300)
-w.setWindowTitle("Resume Builder")
+class IntroWindow(QMainWindow, form):
+    def __init__(self):
+        super(IntroWindow, self).__init__()
+        self.setupUi(self)
 
-def btn_callback():
-    print("Quit..")
-    app.quit()
-
-
-b = QPushButton("Quit", w)
-b.resize(200, 100)
-b.move(650, 1300)
-b.clicked.connect(btn_callback)
-
-
-
-w.show()
-
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    w = IntroWindow()
+    w.show()
+    sys.exit(app.exec_())
