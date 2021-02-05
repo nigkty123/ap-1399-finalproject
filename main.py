05b4f57ed6339bc24030a944815caac8f54c45c2
mrk7711 <mohammadreza7711@yahoo.com>
2021-02-05 16:06:29 +0330

    First commit by MRK

--- main.py
+++ main.py
@@ -6,29 +6,20 @@ from PyQt5 import uic,QtCore
 
 form1 = uic.loadUiType(os.path.join(os.getcwd(), "form.ui"))[0]
 form2 = uic.loadUiType(os.path.join(os.getcwd(), "form2.ui"))[0]
-form3 = uic.loadUiType(os.path.join(os.getcwd(), "form3.ui"))[0]
 
-class IntroWindow3(QMainWindow,form3):
-    def __init__(self):
-        super(IntroWindow3, self).__init__()
-        self.setupUi(self)
-        self.NextButton.clicked.connect(lambda: self.close())
         
-class IntroWindow2(QMainWindow,form2):
+class IntroWindow2(QMainWindow,form2):  
     def __init__(self):
         super(IntroWindow2, self).__init__()
         self.setupUi(self)
-        self.NextButton.clicked.connect(self.create)
-    def create(self):
-        self.t=IntroWindow3()   
-        self.t.show()
-        self.close()
+        self.NextButton.clicked.connect(lambda: self.close())
+       
         
 class IntroWindow1(QMainWindow,form1):
     def __init__(self):
         super(IntroWindow1, self).__init__()
         self.setupUi(self)
- 
+        
         self.CreatButton.clicked.connect(self.create)
     def create(self):
         self.t=IntroWindow2()
