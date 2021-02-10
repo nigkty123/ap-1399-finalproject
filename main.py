976039cc1c656a91f7be5b535008472754e1d188
Negin Naserzadeh <72472009+nigkty123@users.noreply.github.com>
2021-02-11 00:40:09 +0330

    idk how to give user input values to the html file

--- main.py
+++ main.py
@@ -1,93 +1,106 @@
 import sys
 import os
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QFrame
-from fpdf import FPDF
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QFormLayout
 from PyQt5.QtCore import QFileInfo
 from PyQt5.QtPrintSupport import QPrinter
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import inch
+from reportlab.pdfgen.canvas import Canvas
+from fpdf import FPDF, HTMLMixin
 
 form = uic.loadUiType(os.path.join(os.getcwd(), "form.ui"))[0]
+
+       
+class HTML2PDF(FPDF, HTMLMixin):
+        pass
+
 
 class IntroWindow(QMainWindow, form):
     def __init__(self):
         super(IntroWindow, self).__init__()
         self.setupUi(self)
-        self.textEdit = QTextEdit(self)
-        self.PrintButton.clicked.connect(self.PrintPDF)
+        self.PrintButton.clicked.connect(self.html2pdf)
         self.QuitButton.clicked.connect(self.exit)
+        Name = self.nameLineEdit.text()
+        email = self.emailLineEdit.text()
+        education = self.educationLineEdit.text()
+        experience = self.experienceLineEdit.text()
+        nCAP = self.nCAPLineEdit.text()
+        lan = self.languagesLineEdit.text()
+        intrests = self.hobbiesLineEdit.text()
+        about = self.descriptionLineEdit.text()
+        job = self.jobLineEdit.text()
+
+
+        
+        
     def exit(self):
         app.quit()  
-    def PrintPDF(self):
-        fn, _ = QFileDialog.getSaveFileName(self, 'print', None, 'Pdf files (.pdf);;All Files()')
-        if fn != '':
-            if QFileInfo(fn).suffix() =="" : fn += '.pdf'
-
-            printer = QPrinter(QPrinter.HighResolution)
-            printer.setOutputFormat(QPrinter.PdfFormat)
-            printer.setOutputFileName(fn)
-            self.textEdit.document().print_(printer)
 
 
+    def html2pdf(self ,Name= None, lastname= None, address= None, email= None, education= None, experience= None,nCAp= None, lan= None, intrests= None,about= None, job = None):
+        html = ''' <div id="header"></div>
+        <div class="left"></div>
+        <div class="stuff">
+        <br><br>
+        <h1>Resume</h1>
+        <h2>Emily</h2>
+        <hr />
+        <br>
+        <p class="head">Interests</p>
+        <ul>
+            <li>{self.}</li>
+            
+        </ul>
+        <p class="head">Skills</p>
+        <ul>
+            <li>Web Design with HTML & CSS</li>
+        </ul>
+        <p class="head">Education</p>
+        <ul>
+            <a href="http://www.wiltonhighschool.org/pages/Wilton_High_School">
+            <li>Wilton High School</li>
+            </a>
+            <!--Link-->
+            <a href="https://www.silvermineart.org/">
+            <li>Silvermine School of Arts</li>
+            </a>
+            <li>Codeacademy</li>
+        </ul>
+        <p class="head">Experience</p>
+        <ul>
+            <li>Student Technology Intern for Wilton School District</li>
+            <li>Babysitter</li>
+        </ul>
+        <p class="head">Extracurriculars</p>
+        <ul>
+            <li>Recycling Club</li>
+            <li>Gardening Club</li>
+            <li>Book Club</li>
+        </ul>
+        </div>
+        <div class="right"></div>
+        <div id="footer">
+        <h2 id="name">Emily</h2></div>
+
+    '''
+
+        pdf = HTML2PDF()
+        #First page
+        pdf.add_page()
+        pdf.write_html(html)
+        pdf.output('Resume.pdf', 'F')
+
+    
+    
 
 
-title = '20000 Leagues Under the Seas'
 
-class PDF(FPDF):
-    def header(self):
-        # Arial bold 15
-        self.set_font('Arial', 'B', 15)
-        # Calculate width of title and position
-        w = self.get_string_width(title) + 6
-        self.set_x((210 - w) / 2)
-        # Colors of frame, background and text
-        self.set_draw_color(0, 80, 180)
-        self.set_fill_color(230, 230, 0)
-        self.set_text_color(220, 50, 50)
-        # Thickness of frame (1 mm)
-        self.set_line_width(1)
-        # Title
-        self.cell(w, 9, title, 1, 1, 'C', 1)
-        # Line break
-        self.ln(10)
 
-    def footer(self):
-        # Position at 1.5 cm from bottom
-        self.set_y(-15)
-        # Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Text color in gray
-        self.set_text_color(128)
-        # Page number
-        self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'C')
 
-    def chapter_title(self, num, label):
-        # Arial 12
-        self.set_font('Arial', '', 12)
-        # Background color
-        self.set_fill_color(200, 220, 255)
-        # Title
-        self.cell(0, 6, 'Chapter %d : %s' % (num, label), 0, 1, 'L', 1)
-        # Line break
-        self.ln(4)
 
-    def chapter_body(self, name):
-        # Read text file
-        with open(name, 'rb') as fh:
-            txt = fh.read().decode('latin-1')
-        # Times 12
-        self.set_font('Times', '', 12)
-        # Output justified text
-        self.multi_cell(0, 5, txt)
-        # Line break
-        self.ln()
-        # Mention in italics
-        self.set_font('', 'I')
-        self.cell(0, 5, '(end of excerpt)')
-
-    def print_chapter(self, num, title, name):
-        self.add_page()
-        self.chapter_title(num, title)
-        self.chapter_body(name)        
+       
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -95,9 +108,3 @@ if __name__ == "__main__":
     w = IntroWindow()
     w.show()
     sys.exit(app.exec_())
-    pdf = PDF()
-    pdf.set_title(title)
-    pdf.set_author('Jules Verne')
-    pdf.print_chapter(1, 'A RUNAWAY REEF', '20k_c1.txt')
-    pdf.print_chapter(2, 'THE PROS AND CONS', '20k_c2.txt')
-    pdf.output('tuto3.pdf', 'F')
