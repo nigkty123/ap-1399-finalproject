b93ad2c70c773fa3535545311c7744efdc5c687f
Negin Naserzadeh <72472009+nigkty123@users.noreply.github.com>
2021-02-11 20:18:00 +0330

    puts inputs in html file it's just ugly

--- main.py
+++ main.py
@@ -22,15 +22,7 @@ class IntroWindow(QMainWindow, form):
         self.setupUi(self)
         self.PrintButton.clicked.connect(self.html2pdf)
         self.QuitButton.clicked.connect(self.exit)
-        Name = self.nameLineEdit.text()
-        email = self.emailLineEdit.text()
-        education = self.educationLineEdit.text()
-        experience = self.experienceLineEdit.text()
-        nCAP = self.nCAPLineEdit.text()
-        lan = self.languagesLineEdit.text()
-        intrests = self.hobbiesLineEdit.text()
-        about = self.descriptionLineEdit.text()
-        job = self.jobLineEdit.text()
+        
 
 
         
@@ -39,50 +31,82 @@ class IntroWindow(QMainWindow, form):
         app.quit()  
 
 
-    def html2pdf(self ,Name= None, lastname= None, address= None, email= None, education= None, experience= None,nCAp= None, lan= None, intrests= None,about= None, job = None):
-        html = ''' <div id="header"></div>
-        <div class="left"></div>
-        <div class="stuff">
-        <br><br>
-        <h1>Resume</h1>
-        <h2>Emily</h2>
-        <hr />
-        <br>
-        <p class="head">Interests</p>
-        <ul>
-            <li>{self.}</li>
-            
+    def html2pdf(self ):
+        
+        Name = self.nameLineEdit.text()
+        email = self.emailLineEdit.text()
+        address = self.emailLineEdit.text()
+        phone = self.emailLineEdit.text()
+        education = self.educationLineEdit.text()
+        experience1 = self.experience1LineEdit.text()
+        experience2 = self.experience2LineEdit.text()
+        experience3 = self.experience3LineEdit.text()
+        nCAP = self.nCAPLineEdit.text()
+        lan = self.languagesLineEdit.text()
+        intrests = self.hobbiesLineEdit.text()
+        about = self.descriptionLineEdit.text()
+        job = self.jobLineEdit.text()
+        skills = self.skillsLineEdit.text()
+        refrences = self.refrencesLineEdit.text()
+        
+        html = f''' <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic|Open+Sans:300,400,500,700|Waiting+for+the+Sunrise|Shadows+Into+Light' rel='stylesheet' type='text/css'>
+
+<div class="wrapper clearfix">
+<div class="left">
+    
+    <div class="name-hero">
+      
+      <div class="me-img"></div>
+      
+      <div class="name-text">
+        
+        <h1>{Name}</em></h1>
+        <p>{address}</p>
+        <p>{email}/p> 
+        <p>({phone}</p>
+     
+      </div>
+   
+    </div>
+    
+  </div>
+ <div class="right">
+    
+    <div class="inner">
+      <section>
+        <h1>Employment</h1>
+        <p>{experience1}</em></p>
+        <p>{experience2}</em></p>
+        <p>{experience3}</em></p>
+      </section>
+      <section>
+        <h1>Technical Skills</h1>
+        <ul class="skill-set">
+          <li>{skills}</li>
         </ul>
-        <p class="head">Skills</p>
-        <ul>
-            <li>Web Design with HTML & CSS</li>
+      </section>
+      <section>
+        <h1>References</h1>
+        <p>{refrences}</em></p>
+      </section>
+            <section>
+        <h1>Personal Interests</h1>
+        <ul class="skill-set">
+          <li>{intrests}</li>
         </ul>
-        <p class="head">Education</p>
-        <ul>
-            <a href="http://www.wiltonhighschool.org/pages/Wilton_High_School">
-            <li>Wilton High School</li>
-            </a>
-            <!--Link-->
-            <a href="https://www.silvermineart.org/">
-            <li>Silvermine School of Arts</li>
-            </a>
-            <li>Codeacademy</li>
-        </ul>
-        <p class="head">Experience</p>
-        <ul>
-            <li>Student Technology Intern for Wilton School District</li>
-            <li>Babysitter</li>
-        </ul>
-        <p class="head">Extracurriculars</p>
-        <ul>
-            <li>Recycling Club</li>
-            <li>Gardening Club</li>
-            <li>Book Club</li>
-        </ul>
+      </section>
+      <section>
+        <div class="handmade">
+          <p>handmade by <em> {Name}</em></p>
         </div>
-        <div class="right"></div>
-        <div id="footer">
-        <h2 id="name">Emily</h2></div>
+      </section>
+    </div>
+    
+  </div>
+  
+  
+  
+</div>
 
     '''
 
